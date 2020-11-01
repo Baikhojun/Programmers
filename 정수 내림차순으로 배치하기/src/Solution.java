f306f9cc9e3c41797830f09b6ff08cdde9f117cc
Baik Ho june <qorghwns90@naver.com>
2020-11-01 22:32:58 +0900

    2020-11-01

---
+++ 정수 내림차순으로 배치하기/src/Solution.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Solution {
+	public static void main(String[] args) {
+		
+		long n = 118372;
+	    String str = String.valueOf(n);
+        int len = str.length();
+        int [] arr = new int[len];
+        long answer = 0;
+        
+        for(int i=0; i< len ; i++)
+        {
+            arr[i]= (int)(n%10);
+            n/=10;
+        }
+        Arrays.sort(arr,Collections.reverseOrder());
+	
+	}
+}
